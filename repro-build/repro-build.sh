fbbf50001776533269518c27b0a50e9ac8971618
Kevin Wang <wy721@qq.com>
2024-12-08 03:27:13 +0000

    wip

--- repro-build/repro-build.sh
+++ repro-build/repro-build.sh
@@ -41,7 +41,7 @@ rm -rf .dummy
 build_to() {
     mkdir -p $1
     BUILD_CMD="${2} ${GUEST_SRC_DIR}/build.sh guest ./bb-build"
-    docker run --rm \
+    docker run --platform linux/amd64 --rm \
         --user $(id -u):$(id -g) \
         -v $REPO_ROOT:$GUEST_SRC_DIR \
         -v $1:$GUEST_BUILD_DIR \
@@ -64,8 +64,8 @@ fi
 echo "## Reproducible build command:"
 echo '```bash'
 echo "git clone https://github.com/Dstack-TEE/meta-dstack.git"
-echo "cd meta-dstack/repro-build"
+echo "cd meta-dstack/"
 echo "git checkout $(git -C $GIT_DIR rev-parse HEAD)"
 echo "git submodule update --init --recursive"
-echo "./repro-build.sh --no-check"
+echo "cd repro-build && ./repro-build.sh --no-check"
 echo '```'
